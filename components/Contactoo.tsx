f694206e167ab9070c192d4e62b6dfe3ddfd1433
peppeferd <giuseppeferlazzo@outlook.it>
2024-02-08 16:07:50 +0100

    Last commit

--- components/Contactoo.tsx
+++ components/Contactoo.tsx
@@ -20,47 +20,47 @@ const Contactoo: FC = () => {
   }
 
   return (
-    <div className="mx-2">
+    <div className="text-center justify-center items-center mt-4">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-5">
+        <div className="mb-5 text-center justify-center items-center flex flex-col">
           <label
             htmlFor="name"
-            className="mb-3 block text-base font-medium text-black"
+            className="mb-3 block text-base font-medium text-black bg-gray-400 w-fit p-3 rounded-md"
           >
             Name
           </label>
           <input
             type="text"
             placeholder="Please enter your name"
-            className="lg:w-[30%] rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
+            className="p-2 lg:w-[20%] h-[35%] rounded-md"
             {...register('name', { required: true })}
           />
         </div>
-        <div className="mb-5">
+        <div className="mb-5 text-center justify-center items-center flex flex-col">
           <label
             htmlFor="email"
-            className="mb-3 block text-base font-medium text-black"
+            className="mb-3 block text-base font-medium text-black bg-gray-400 w-fit p-3 rounded-md"
           >
             Email
           </label>
           <input
             type="email"
             placeholder="Please enter your email"
-            className="lg:w-[30%] rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
+            className="p-2 lg:w-[20%] h-[35%] rounded-md"
             {...register('email', { required: true })}
           />
         </div>
-        <div className="mb-5">
+        <div className="mb-5 text-center justify-center items-center flex flex-col">
           <label
             htmlFor="message"
-            className="mb-3 block text-base font-medium text-black"
+            className="mb-3 block text-base font-medium text-black bg-gray-400 w-fit p-3 rounded-md"
           >
             Message
           </label>
           <textarea
             rows={4}
             placeholder="Please enter your message"
-            className="lg:w-[30%] resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
+            className="p-2 lg:w-[50%] h-[35%] rounded-md"
             {...register('message', { required: true })}
           ></textarea>
         </div>
